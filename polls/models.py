679febf7ef2700df4a2389f0a00df82102662d8f
Jaroslav Pesek <jaroslav.pesek@icloud.com>
2021-03-08 00:13:21 +0100

    init

---
+++ polls/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+ 
+# Create your models here.
+class Poem(models.Model):
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
